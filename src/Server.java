8c39cf194cb568bb9ea3340c0b02c40b2c5a3090
endaoco <endaoco@gmail.com>
2019-02-11 18:52:40 +0000

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,44 @@
-package src;
+import java.io.*;
+import java.net.*;
 
 public class Server {
+
+   public static void main(String[] args) {
+
+       Socket s = null;
+       PrintWriter pw = null;
+       BufferedReader br = null;
+       String str;
+
+        try {
+            ServerSocket server = new ServerSocket(4444);
+            s = server.accept();
+            pw = new PrintWriter(s.getOutputStream(), true);
+            br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            System.out.println("Connected");
+        } catch (java.io.IOException ex) {
+            System.out.println("Not connected");
+        }
+
+
+        try{
+            while ((str = br.readLine()) != null) {
+                System.out.println("The message: " + str);
+
+                if (str.equals("bye")) {
+                    pw.println("bye");
+                    break;
+                } else {
+                    str = "Server returns " + str;
+                    pw.println(str);
+                }
+            }
+            pw.close();
+            br.close();
+
+            s.close();
+        } catch(java.io.IOException ex){
+
+        }
+   }
 }
