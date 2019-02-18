238cf471be3b626d4b7ae87722b9f9eac1738f25
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2019-02-18 17:49:41 +0000

    Updates to server code

--- server/src/main/java/coffeehouse/ServerApplication.java
+++ server/src/main/java/coffeehouse/ServerApplication.java
@@ -1,8 +1,47 @@
 package coffeehouse;
 
+import java.io.*;
+import java.net.*;
+
 public class ServerApplication
 {
 	public static void main(String[] args)
 	{
+
+       Socket s = null;
+       PrintWriter pw = null;
+       BufferedReader br = null;
+       String str;
+
+        try {
+            ServerSocket server = new ServerSocket(4444);
+            s = server.accept();
+            pw = new PrintWriter(s.getOutputStream(), true);
+            br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            System.out.println("Connected");
+        } catch (java.io.IOException ex) {
+            System.out.println("Not connected");
+        }
+
+
+        try{
+            while ((str = br.readLine()) != null) {
+                System.out.println("The message: " + str);
+
+                if (str.equals("bye")) {
+                    pw.println("bye");
+                    break;
+                } else {
+                    str = "Server returns " + str;
+                    pw.println(str);
+                }
+            }
+            pw.close();
+            br.close();
+
+            s.close();
+        } catch(java.io.IOException ex){
+
+        }
 	}
 }
