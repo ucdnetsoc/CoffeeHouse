e9092dccb6fa6848144b8205b5e5b9cfd8bccdd0
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2019-03-26 15:52:53 +0000

    Fixes

--- client/src/main/java/coffeehouse/ClientApplication.java
+++ client/src/main/java/coffeehouse/ClientApplication.java
@@ -1,20 +1,10 @@
 package coffeehouse;
 
-import coffeehouse.net.Client;
-import coffeehouse.util.IOUtils;
+import coffeehouse.gui.LoginController;
 import javafx.application.Application;
-
-import java.io.IOException;
-import java.net.URL;
-import java.net.UnknownHostException;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class ClientApplication extends Application {
@@ -25,16 +15,27 @@ public class ClientApplication extends Application {
 	@Override
 	public void start(Stage primaryStage) {
         Parent root;
+        LoginController controller;
         try {
-            URL resourceUrl = getClass().getResource("/coffeehouse/gui/Login.fxml");
-            root = FXMLLoader.load(resourceUrl);
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("gui/Login.fxml"));
+            root = fxmlLoader.load();
+            controller = fxmlLoader.getController();
         } catch(Exception e) {
             System.err.println("Exception while fetching Login.fxml");
             e.printStackTrace();
             return;
         }
+        if(controller != null)
+            controller.setClientApp(this);
+        else
+            System.err.println("controller is null");
 
 		primaryStage.setScene(new Scene(root, 300, 250));
 		primaryStage.show();
 	}
+
+	public void login(String ip, int port, String username)
+    {
+        // TODO: implement
+    }
 }
--- client/src/main/java/coffeehouse/gui/LoginController.java
+++ client/src/main/java/coffeehouse/gui/LoginController.java
@@ -1,8 +1,11 @@
 package coffeehouse.gui;
 
+import coffeehouse.ClientApplication;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 
 public class LoginController {
 	@FXML
@@ -13,4 +16,33 @@ public class LoginController {
 	
 	@FXML
 	private Button loginButton;
+
+	private ClientApplication clientApp;
+
+	@FXML
+	public void initialize() {
+		loginButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				String ip = serverIpField.getText();
+				String username = usernameField.getText();
+
+				// TODO: Check if ip and username are valid format
+
+				int port = 3000;
+
+				// TODO: if ip contains port (e.g. 192.168.0.10:3000) use that instead
+
+				loginButton.setText("Clicked"); // Something to show that this was run
+
+				clientApp.login(ip, port, username);
+			}
+		});
+	}
+
+	public void setClientApp(ClientApplication clientApp) {
+		this.clientApp = clientApp;
+	}
+
+
 }
